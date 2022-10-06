779910c76cce4d8304d5cef66dd40febf914bd1c
German Villalba <german@Germans-MacBook-Air.local>
2022-10-06 11:23:47 -0400

    Add manifest config to run the main class as a Jar

--- src/main/java/com/frontlineeducation/codesample/germanvillalba/solver/FrontlineEducationSolver.java
+++ src/main/java/com/frontlineeducation/codesample/germanvillalba/solver/FrontlineEducationSolver.java
@@ -32,6 +32,6 @@ public class FrontlineEducationSolver implements IFrontlineEducationRequirements
     }
 
     public static void main(String[] args) {
-
+        System.out.println("Frontline Education Solver");
     }
 }
